7005284ea6db27d37db0efba6042f0aa188a9821
Dan Staggs <Dan@Dans-MacBook-Pro.local>
2018-11-13 00:09:09 -0600

    mix of psuedocode and rough JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+// var name = target number array pool(19-120)
+
+// var targetNumber is a blank placeholder array/string that we can populate with a math function further down the code, so that it can be repeated without reloading the pages
+var targetNumber = " "
+
+// var name = crystal value array pool 
+
+// the vars for the crystal values(1-12)
+// should I set this as: var = crystalValues [a, b, c, d]? I think I should.
+var = crystal1
+var = crystal2
+var = crystal3
+var = crystal4
+
+// var clickSum = hold the sum of guesses
+
+
+var winsCounter = 0;
+var lossCounter = 0;
+
+
+// the following three functions (startGame, addValues, and roundComplete) mimic the example from Monday Nov 12 that showed the game loop.
+function startGame(){
+    clickSum = 0;
+    // populate crystal-1
+    // populate crystal-2 
+}
+
+function addValues(){}
+
+function roundComplete(){}
+
+
+// start game gets the game going, now that all the terms have been defined
+startGame();
+
+document.onclick = function(event){
+    // sum of crystals
+    // is value over the targetNumber
+    // round complete
+
+}
