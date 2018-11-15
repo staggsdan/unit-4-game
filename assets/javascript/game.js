b6e7226211a9402a5ce64ae377b57972f9bfca86
Dan Staggs <Dan@Dans-MacBook-Pro.local>
2018-11-14 21:43:45 -0600

    broadly functional but some odd/loose behaviors. debugging.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,68 @@
 var targetNumber = 0;
-
-// var clickSum = hold the sum of guesses
 var clickSum = 0;
-
-
 var winsCounter = 0;
 var lossCounter = 0;
 
-function resetHTML
+function getRndInteger(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min)}
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-    }
-
-function assignCrystalValue(){console.log("hello")
+function assignCrystalValue(){
     $("#crystals").children("h1").each(function(index, item){
-        $(item).data("value", getRndInteger(1,12))
-    })
-}
-
-$('h1').on('click', function () {
-    console.log($(this).data("value"))
-
-})
-assignCrystalValue()
-
+        $(item).attr("value", getRndInteger(1,12));
+        $(item).addClass("clickZone");
+        })
+    }
 
 // the following three functions (startGame, addValues, and roundComplete) mimic the example from Monday Nov 12 that showed the game loop.
+
 function startGame(){
     clickSum = 0;
-    assignCrystalValue()
-    // populate target number
-    
-    targetNumber = getRndInteger(19, 120)
-        console.log(targetNumber)
-
-    // for (let crystalValues = 0; crystalValues < 4; crystalValues++) {
-    //     getRndInteger(1,12);
-    //     } 
-    // (crystalValues < 4)[
-    //     getRndInteger(1,12)
+    assignCrystalValue();
+    targetNumber = getRndInteger(19,120)
     }
-
-// office hours: code from 30 to 38. target number works. what is the exact syntax for populating the crystalValues array with four index positions, using the function (argument): getRndInteger(1,12)? 
-// crystalValues.push(getRndInteger(1,12))
-
-
-function addValues(){
-
+    
+function roundComplete(){
+    if (clickSum > targetNumber){
+        document.getElementById("win-lose").innerHTML = "you lose";
+        lossCounter++;
+        startGame();
+    }
+    if (clickSum === targetNumber){
+        document.getElementById("win-lose").innerHTML = "you win!";
+        winsCounter++;
+        startGame();
+    }
 }
 
-function roundComplete(){
-//     if (clickSum > targetNumber){
-        
-//         document.getElementById("win-lose").text = "you lose";
-//         lossCounter++;
-//         startGame();
-//     }else if (clickSum === targetNumber){
-        
-//         document.getElementById("win-lose").text = "you win!";
-//         winsCounter++;
-//         startGame();
-//     }
-//     // if the points ===, trigger win
-//     // if the points are under, allow game to continue. I guess there wouldn't even be code for that?
-// }
-
-
 // start game gets the game going, now that all the terms have been defined
-// startGame()
+startGame()
 
-// document.onclick = function(event)
-    // sum of crystals
-    // is value over the targetNumber
-    // round complete
-
+document.getElementById("target-number").innerHTML = targetNumber;
+document.getElementById("win-tally").innerHTML = "Wins: " + winsCounter;
+document.getElementById("loss-tally").innerHTML = "Losses: " + lossCounter;
 
 
+$(".clickZone").on("click", function() {
+    var crystalValue = ($(this).attr("value"));
+    var crystalValueInt = parseInt(crystalValue);
+    clickSum += crystalValueInt;
+    document.getElementById("points-tally").innerHTML = clickSum;
+    roundComplete()
+
+    // if (clickSum === targetNumber) {
+        
+    //     document.getElementById("win-lose").text = "you win!";
+    //     winsCounter++;
+    //     startGame()
+    //   }
+  
+    //   if (clickSum > targetNumber) {
+    //     document.getElementById("win-lose").text = "you lose";
+    //     lossCounter++;
+    //     startGame()
+    //   }
+})
+
+// roundComplete()
 
 
-
-    // var name = target number array pool(19-120)
-
-// var targetNumber is a blank placeholder array/string that we can populate with a math function further down the code, so that it can be repeated without reloading the pages
